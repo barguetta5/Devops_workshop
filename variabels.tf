1dceec6a2936baf180fe4aece1aa505d4260a895
barguetta5 <kooky15963@gmail.com>
2025-02-15 19:21:44 +0200

    Add eks module, cluster, ec2, and node group

--- variabels.tf
+++ variabels.tf
@@ -11,6 +11,3 @@ variable "subnet_ids" {
   description = "list of all the subnets"
 }
 
-# iam_number     = "arn:aws:iam::730335218716:user/barg-user "
-# subnet_ids     = ["subnet-0cd1f4e5f3d085c55", "subnet-0339180eebd96ac4a"]
-# vpc_id = "vpc-01b834daa2d67cdaa"
